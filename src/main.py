ab390b0f0e6d1e3dff1f7861cee460a7ee39514c
mrhatted <bramsterkendries@gmail.com>
2025-06-02 14:35:58 +0200

    about to start messing stuff up

--- src/main.py
+++ src/main.py
@@ -30,9 +30,11 @@ def main():
     #print (node)
     #print(f"result of split delimeter={split_nodes_delimeter([node], "`", TextType.code)}")
     #print(f"result of split delimeter={split_nodes_delimeter([node2], "**", TextType.bold)}")
-    text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
-    print(extract_markdown_images(text))
-    text2 = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
-    print(extract_markdown_links(text2))
+    #text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+    #print(extract_markdown_images(text))
+    #text2 = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+    #print(extract_markdown_links(text2))
+    node3 = TextNode("This is text with a link ![to boot dev](https://www.boot.dev)",TextType.text)
+    print(split_nodes_image([node3]))
 
 main()
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -32,5 +32,23 @@ class TestTextNode(unittest.TestCase):
         )
         self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
 
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.text,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.text),
+                TextNode("image", TextType.image, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.text),
+                TextNode(
+                    "second image", TextType.image, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
 if __name__ == "__main__":
     unittest.main()
