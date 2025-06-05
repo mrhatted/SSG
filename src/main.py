7bafd15d3c5b52e38f250f7ef678b5b61a3d8148
mrhatted <bramsterkendries@gmail.com>
2025-06-05 16:49:40 +0200

    not taking files

--- src/main.py
+++ src/main.py
@@ -110,7 +110,7 @@ def main():
     
     
     shutil.rmtree("{basepath}docs",ignore_errors=True)
-    copy_src_to_target(f".{basepath}static",f".{basepath}docs")
+    copy_src_to_target(f"{basepath}static",f"{basepath}docs")
     generate_page(f".{basepath}content/index.md", f".{basepath}template.html", f".{basepath}docs/index.html")
     generate_pages_recursive(f".{basepath}content",f".{basepath}template.html",f".{basepath}docs")
     
